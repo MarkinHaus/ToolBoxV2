b3f4cde40b2e3b789c2e270bc441711cc313ef7e
Markin <markinhausmanns@gmail.com>
2023-01-02 17:22:02 +0100

    new setup.cfg

--- toolboxv2/__init__.py
+++ toolboxv2/__init__.py
@@ -6,5 +6,5 @@ from toolboxv2.app.serve_app import AppServerHandler
 
 __author__ = """Markin Hausmanns"""
 __email__ = 'Markinhausmanns@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = [App, MainTool, FileHandler, Style, run_cli, AppServerHandler, AppArgs]
