6e7f334775a709f9bbbf9d7abebbff79adecfcbd
Markin <markinhausmanns@gmail.com>
2023-12-13 12:25:29 +0100

    __init__.py mods remove circular input  utils -> add types and classes (all_functions_enums.py)
      main exports
       add get_app stable
       tbef for  all_functions_enums for better calling
       add Result a recurse result type

--- toolboxv2/__init__.py
+++ toolboxv2/__init__.py
@@ -1,24 +1,23 @@
 """Top-level package for ToolBox."""
 from toolboxv2.utils.Style import Style, remove_styles, Spinner
 from toolboxv2.utils.file_handler import FileHandler
-from toolboxv2.utils.toolbox import App, AppArgs
+from toolboxv2.utils.toolbox import App, AppArgs, get_app
 from toolboxv2.utils.tb_logger import setup_logging, get_logger
-
 from toolboxv2.utils.main_tool import MainTool
-
+from toolboxv2.utils import all_functions_enums as tbef
+from toolboxv2.utils import Result
 from toolboxv2.runabel import runnable_dict
 
-try:
-    MODS_ERROR = None
-    import toolboxv2.mods
-    from toolboxv2.mods import *
-except ImportError as e:
-    MODS_ERROR = e
-
+# try:
+#     MODS_ERROR = None
+#     import toolboxv2.mods
+#     from toolboxv2.mods import *
+# except ImportError as e:
+#     MODS_ERROR = e
 
 __author__ = """Markin Hausmanns"""
 __email__ = 'Markinhausmanns@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.6'
 __all__ = [
     "__version__",
     "App",
@@ -32,7 +31,10 @@ __all__ = [
     "get_logger",
     "runnable_dict",
     "mods",
-    "utils"
+    "utils",
+    "get_app",
+    "tbef",
+    "Result",
     ]
 
 ToolBox_over: str = "root"
--- toolboxv2/mods/__init__.py
+++ toolboxv2/mods/__init__.py
@@ -1,3 +1,2 @@
-from toolboxv2 import *
 from toolboxv2.utils.toolbox import get_app
 BROWSER = 'firefox'  # 'chrome'
