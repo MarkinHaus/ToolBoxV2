5281c884cdc9064d8c88abf42379274932ea1ace
Markin <markinhausmanns@gmail.com>
2025-05-13 15:55:36 +0200

    ruff checks, Tauri=False,

--- .github/hooks/setup_hooks.sh
+++ .github/hooks/setup_hooks.sh
@@ -7,3 +7,9 @@ GIT_HOOKS_DIR=".git/hooks"
 
 cp "$HOOKS_DIR/pre-commit.sh" "$GIT_HOOKS_DIR/pre-commit"
 chmod +x "$GIT_HOOKS_DIR/pre-commit"
+
+cp "$HOOKS_DIR/prepare-commit-msg.sh" "$GIT_HOOKS_DIR/prepare-commit-msg"
+chmod +x "$GIT_HOOKS_DIR/prepare-commit-msg"
+
+cp "$HOOKS_DIR/common_quality_utils.sh" "$GIT_HOOKS_DIR/common_quality_utils.sh"
+
